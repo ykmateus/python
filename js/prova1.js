8b078995c8a310e4a3a61a100830682b6796b999
ykmateus <146597573+ykmateus@users.noreply.github.com>
2024-06-14 18:19:49 -0300

    Add files via upload

---
+++ js/prova1.js
@@ -0,0 +1,14 @@
+// Declaração das notas e pesos
+let n1 = 8.5;
+let n2 = 7.0;
+let n3 = 9.5;
+
+let p1 = 2;
+let p2 = 3;
+let p3 = 5;
+
+// Cálculo da média ponderada
+let media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3);
+
+// Exibindo a média ponderada no console
+console.log("A média ponderada é: " + media.toFixed(2));
